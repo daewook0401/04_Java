b7da42396a0fb9b89b6d411014d7622d66b2bacd
daewook0401 <daewook@gimdaeug-ui-MacBookPro.local>
2025-01-20 17:29:46 +0900

    0120

--- 00_JavaTest/src/Test1.java
+++ 00_JavaTest/src/Test1.java
@@ -7,7 +7,6 @@
  * 2) 컴파일 완료 후 .class 파일 실행 (java class파일명)
  */
 
-
 public class Test1 {
 
 	public static void main(String[] args) {
@@ -17,9 +16,12 @@ public class Test1 {
 		System.out.println("타자 연습");
 		System.out.println("으잉");
 		
-		/* Java API : Java에서 제공해주는 코드의 사용
-		 * 
-		 * 
+		/* Java API : Java에서 제공해주는 코드의 사용법이 설명 되어있는 문
+		 *
+		 *  DOC 확인 방법
+		 *  1) google -> java 21 api 검
+		 *  
+		 *  2) 궁금한 코드 클릭 -> shift + f2
 		 */
 		
 	}
