7ff5cf4731b71ee46790a28df910dbadab30d5c5
daewook0401 <daewook@gimdaeug-ui-MacBookPro.local>
2025-01-20 11:25:12 +0900

    first

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,27 @@
+// JS와 주석 동일 
+
+// Run 버튼 클릭 시 (축키 ctrl + f11)
+/* 1) 자동으로 컴파일 수행(javac ooo.java)
+ * 	-> .class 파일(byte code) 생성
+ * 
+ * 2) 컴파일 완료 후 .class 파일 실행 (java class파일명)
+ */
+
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이클립스 실행 중 ");
+		System.out.println("자꾸 그러지마....");
+		System.out.println("타자 연습");
+		System.out.println("으잉");
+		
+		/* Java API : Java에서 제공해주는 코드의 사용
+		 * 
+		 * 
+		 */
+		
+	}
+
+}
---
+++ JavaTest.java
@@ -0,0 +1,8 @@
+public class JavaTest{
+  public static void main(String[] args){
+    System.out.println("Hello World!!");
+    for(int i=1; i<=5; i++){
+      System.out.println(i + "번 째 줄 출력");
+    }
+  }
+}
