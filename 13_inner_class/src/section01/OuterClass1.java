7984b2d207a75f217f86107a22df8baef916ca22
daewook0401 <daewook@gimdaeug-ui-MacBookPro.local>
2025-02-12 10:52:56 +0900

    12_io

--- 13_inner_class/src/section01/OuterClass1.java
+++ 13_inner_class/src/section01/OuterClass1.java
@@ -7,4 +7,10 @@ public class OuterClass1 {
 	private void test() {
 		System.out.println("OuterClass1의 test() 메서드 입니다");
 	}
+	
+	public class InstanceInnerClass{
+		public void display() {
+			System.out.println("InstanceInnerClass의 display() 메서드");
+		}
+	}
 }
