4be696eb3bf3adaaccfd368a39eba8412c284c38
daewook0401 <daewook@gimdaeug-ui-MacBookPro.local>
2025-02-12 17:10:48 +0900

    finish

--- 13_inner_class/src/section01/OuterClass1.java
+++ 13_inner_class/src/section01/OuterClass1.java
@@ -6,11 +6,24 @@ public class OuterClass1 {
 	
 	private void test() {
 		System.out.println("OuterClass1의 test() 메서드 입니다");
+		System.out.println("num : " + num);
 	}
 	
+	/* 외부 클래스 메서드를 이용해서 내부 클래스 객체 생성
+	 * 
+	 */
+	public void displayMessage() {
+		System.out.println("외부 클래스에서 내부 클래스 객체 생성");
+		InstanceInnerClass inner = new InstanceInnerClass();
+		inner.display();
+	}
+	
+	/* 인스턴스 내부 클래스 정의 == OuterClass1의 멤버 */
 	public class InstanceInnerClass{
 		public void display() {
 			System.out.println("InstanceInnerClass의 display() 메서드");
+			num = 300;
+			test();
 		}
 	}
 }
